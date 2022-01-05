42085ed86641ce3a72e1004ff71b26f005d6bac5
petertran98 <Petertran198@gmail.com>
2022-01-05 15:42:39 -0800

    Added anagram solution. Used Regex and split to solve

--- solutions/anagram.js
+++ solutions/anagram.js
@@ -1,1 +1,24 @@
-console.log('Hello world');
+let anagram = (word1, word2) => {
+    //Replaced all non words
+    let replacedWord1 = word1.replace(/[^\w]/gm, '').toLowerCase();
+    let replacedWord2 = word2.replace(/[^\w]/gm, '').toLowerCase();
+
+    if (replacedWord1.length != replacedWord2.length) {
+        return false;
+    }
+
+    for (w of word1) {
+        if (word1.split(w).length - 1 != word2.split(w).length - 1) {
+            return false;
+        }
+    }
+    for (w of word2) {
+        if (word2.split(w).length - 1 != word1.split(w).length - 1) {
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log('Frie2d && Fired2 are anagrams ---------', anagram('Frie2d', 'Fired2'));
+console.log('rue && ruee are anagrams ---------', anagram('rue', 'ruee'));
