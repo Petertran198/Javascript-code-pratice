376a410be90a22cbb50bfd40154d24e48f5dcedd
petertran98 <Petertran198@gmail.com>
2022-01-05 16:10:57 -0800

    Cleaned up anagram function by adding helper method

--- solutions/anagram.js
+++ solutions/anagram.js
@@ -23,3 +23,29 @@ let anagram = (word1, word2) => {
 
 console.log('Frie2d && Fired2 are anagrams ---------', anagram('Frie2d', 'Fired2'));
 console.log('rue && ruee are anagrams ---------', anagram('rue', 'ruee'));
+
+//Cleaned Up Verision------------------------------------------------
+let anagram2 = (word1, word2) => {
+    //Replaced all non words
+    let replacedWord1 = word1.replace(/[^\w]/gm, '').toLowerCase();
+    let replacedWord2 = word2.replace(/[^\w]/gm, '').toLowerCase();
+
+    if (replacedWord1.length != replacedWord2.length) {
+        return false;
+    }
+    loopAndReplace(word1, word2);
+    loopAndReplace(word2, word1);
+
+    return true;
+};
+
+let loopAndReplace = (word1, word2) => {
+    for (let char of word1.replace(/[^\w]/gm, '')) {
+        if (word1.split(char).length - 1 != word2.split(char).length - 1) {
+            return false;
+        }
+    }
+};
+
+console.log('Frie2d && Fired2 are anagrams ---------', anagram2('Frie2d', 'Fired2'));
+console.log('rue && ruee are anagrams ---------', anagram2('rue', 'ruee'));
